7650b9a40c4c431aabd16eda6ef1d1474d02e2c2
adfr96 <alexdellorco@gmail.com>
2019-06-27 22:52:46 +0200

    little fix

--- cservice/src/main/java/it/uniroma3/asw/cservice/controller/CRestController.java
+++ cservice/src/main/java/it/uniroma3/asw/cservice/controller/CRestController.java
@@ -3,7 +3,6 @@ package it.uniroma3.asw.cservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.uniroma3.asw.cservice.domain.CService;
@@ -20,7 +19,7 @@ public class CRestController {
 	}
 	
 	@PostMapping(value="/")
-	public void  saveParametri(@RequestParam String parametri) {
+	public void  saveParametri(String parametri) {
 		cService.saveParametri(parametri);
 	}
 }
