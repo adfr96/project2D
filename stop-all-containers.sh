b814a902b50b0121fe57b08e4580d4866546f536
adfr96 <alexdellorco@gmail.com>
2019-06-30 18:02:36 +0200

    fix script

--- stop-all-containers.sh
+++ stop-all-containers.sh
@@ -1,27 +1,13 @@
 docker stop eureka
-docker stop aservice-1
-docker stop aservice-2
-docker stop aservice-3
-docker stop aservice-4
-docker stop bservice-1
-docker stop bservice-2
-docker stop bservice-3
-docker stop cservice-1
-docker stop cservice-2
-docker stop cservice-3
+docker stop aservice
+docker stop bservice
+docker stop cservice
 docker stop zuul
 
 docker rm eureka
-docker rm aservice-1
-docker rm aservice-2
-docker rm aservice-3
-docker rm aservice-4
-docker rm bservice-1
-docker rm bservice-2
-docker rm bservice-3
-docker rm cservice-1
-docker rm cservice-2
-docker rm cservice-3
+docker rm aservice
+docker rm bservice
+docker rm cservice
 docker rm zuul
 
 docker network rm project2D-net
